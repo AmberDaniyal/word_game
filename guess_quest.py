3eddf2133c4610c2597992a984379c7e9019b491
Amber Daniyal <amberdaniyal@Ambers-Air.fritz.box>
2024-11-15 10:47:35 +0100

    Add word guessing logic and feedback system

--- guess_quest.py
+++ guess_quest.py
@@ -24,3 +24,49 @@ class WordGame:
     def current_state_display(self):
         print("Misplaced letters:", self.misplaced_letters)
         print("Incorrect letters:", self.incorrect_letters )
+
+class GameManager:
+    def __init__(self, max_turns=5):
+        self.max_turns = max_turns
+        self.turns_taken = 0
+        self.word_bank = ["queen", "helps", "learn", "flash", "blaze"]
+        self.game = WordGame(self.word_bank)
+
+    def display_intro(self):
+        print("\nWelcome to Guess Quest!")
+        print(f"The word has {len(self.game.word_to_guess)} letters.")
+        print(f"You have {self.max_turns} turns to guess the word.\n")
+
+    def play_turn(self):
+        guess = input("Guess the word: ").lower()
+
+        if len(guess) != len(self.game.word_to_guess):
+            print(f"Error: Too long! Please enter a {len(self.game.word_to_guess)}-letter word")
+            return False
+
+        self.turns_taken += 1
+
+        if guess == self.game.word_to_guess:
+            print("Congratulations, you win!")
+            return True
+
+        result = self.game.check_guess(guess)
+        print(f"Result: {result}")
+        self.game.current_state_display()
+        return False
+
+    def play(self):
+        self.display_intro()
+
+        while self.turns_taken < self.max_turns:
+            if self.play_turn():
+                return
+            print(f"You have {self.max_turns - self.turns_taken} turns left.\n")
+
+        print(f"\nGame Over! The word was '{self.game.word_to_guess}'")
+
+
+game = GameManager()
+game.play()
+
+
